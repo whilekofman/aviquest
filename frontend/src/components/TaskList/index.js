b6419b8592747f4f11ddffefb68755a4a8cb7f94
Wilson Chen <wilsonchen901@gmail.com>
2022-10-24 18:30:41 -0400

    work on task item

--- frontend/src/components/TaskList/index.js
+++ frontend/src/components/TaskList/index.js
@@ -1,12 +1,13 @@
-import TaskListItem from '../TaskListItem';
 import './TaskList.css';
 
 const TaskList = ({user}) => {
 
     const taskItem = user.tasks.map(task => <TaskListItem key={task.id} task={task}/>)
+    console.log(taskItem);
 
     return ( 
         <div className='tasklist-container'>
+
             <div className='tasklist-body'>
                 <div className='task-input-container'>
                     <form className='task-input-form'>
@@ -15,12 +16,6 @@ const TaskList = ({user}) => {
                         placeholder='Add a task'
                         />
                     </form>
-                </div>
-                
-                <div className='task-list-items'>
-                    <ul>
-                        {taskItem}
-                    </ul>
                 </div>
 
             </div>
--- frontend/src/components/TaskListItem/index.js
+++ frontend/src/components/TaskListItem/index.js
@@ -1,58 +1,11 @@
-import { useState } from 'react';
 import './TaskListItem.css';
 
 const TaskListItem = ({task}) => {
 
-    const [showOptions, setShowOptions] = useState(false);
-    const [options, setOptions] = useState(false);
-    const [checked, setChecked] = useState(false);
-
-    const handleOptions = (e) => {
-        e.preventDefault();
-        setOptions(!options);
-    }
-
     return ( 
-        <div className='task-item-container'
-        onMouseEnter={() => setShowOptions(true)}
-        onMouseLeave={() => setShowOptions(false)}
-        >
-            <div className='task-item-body'>
-                <div className='task-item-body-start'>
-                    <div className='task-item-check'>
-                        <label>
-                            <input type="checkbox"
-                            className='task-checkbox'
-                            onChange={() => setChecked(!checked)}
-                            />
-                            <svg
-                                className={`checkbox ${checked ? "checkbox--active" : ""}`}
-                                aria-hidden="true"
-                                viewBox="0 0 15 11"
-                                fill="none"
-                            > 
-                                <path 
-                                    d="M1 4.5L5 9L14 1"
-                                    strokeWidth="2"
-                                    stroke={checked ? "#fff" : "none"}
-                                />
-                            </svg>
-                        </label>
-                    </div>
-                    <div className='task-item-title'>{task.title}</div>
-                </div>
-                {showOptions && <div className='task-item-options'
-                onClick={(e) => handleOptions(e)}
-                >⋮
-                    {options && 
-                        <div className='task-options-dropdown'
-                        >
-                            <div className='task-options-edit'>✎ &nbsp;  Edit</div>
-                            <div className='task-options-delete'>🗑 Delete</div>
-                        </div>
-                    }
-                </div>}
-            </div>
+        <div className='task-item-container'>
+            <div className='task-item-title'>{task.title}</div>
+            <div className='task-item-options'>⋮</div>
         </div>
      );
 }
