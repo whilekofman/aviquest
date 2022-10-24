119d7b1e29c35b816847c246a7f817a80c4dc407
Eugene Kofman <eugenekofman@gmail.com>
2022-10-24 19:22:11 -0400

    begin session actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import TaskList from "./components/TaskList";
 
 import Navigation from "./components/Navigation";
 import StatusBar from "./components/StatusBar";
+import { Switch } from 'react-router-dom'
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+
+
 
 function App() {
   return (
