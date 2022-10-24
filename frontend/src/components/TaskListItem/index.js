746dc3fb2dddec2ef4ffb7c845e6394b79850c91
Wilson Chen <wilsonchen901@gmail.com>
2022-10-24 19:02:59 -0400

    work on task item

--- frontend/src/components/TaskListItem/index.js
+++ frontend/src/components/TaskListItem/index.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './TaskListItem.css';
 
 const TaskListItem = ({task}) => {
 
+    const [showOptions, setShowOptions] = useState(false);
+
+    const handleOptions = (e) => {
+        e.preventDefault();
+    }
+
     return ( 
-        <div className='task-item-container'>
-            <div className='task-item-title'>{task.title}</div>
-            <div className='task-item-options'>⋮</div>
+        <div className='task-item-container'
+        onMouseEnter={() => setShowOptions(true)}
+        onMouseLeave={() => setShowOptions(false)}
+        >
+            <div className='task-item-body'>
+                <div className='task-item-body-start'>
+                    <div className='task-item-check'><input type="checkbox"/></div>
+                    <div className='task-item-title'>{task.title}</div>
+                </div>
+                {showOptions && <div className='task-item-options'
+                onClick={(e) => handleOptions(e)}
+                >⋮</div>}
+            </div>
         </div>
      );
 }
