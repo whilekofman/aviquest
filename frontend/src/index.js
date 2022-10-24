7f19c56e9daf59b0802f6768d476ccca1a3bfb36
Eugene Kofman <eugenekofman@gmail.com>
2022-10-24 19:22:11 -0400

    begin session actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,20 @@ function Root() {
           <App />
         </BrowserRouter>
   );
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store/store'
+
+let store = configureStore({})
+
+function Root(){
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
 ReactDOM.render(
