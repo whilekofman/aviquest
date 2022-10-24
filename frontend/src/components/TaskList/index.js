ce23f59661fcc0660930b1b91c2acea16bdb8ce3
Wilson Chen <wilsonchen901@gmail.com>
2022-10-24 18:30:41 -0400

    work on task item

--- frontend/src/components/TaskList/index.js
+++ frontend/src/components/TaskList/index.js
@@ -3,10 +3,11 @@ import './TaskList.css';
 
 const TaskList = ({user}) => {
 
-    const taskItem = user.tasks.map(task => <TaskListItem key={task.id} task={task}/>)
+    console.log(user);
 
     return ( 
         <div className='tasklist-container'>
+
             <div className='tasklist-body'>
                 <div className='task-input-container'>
                     <form className='task-input-form'>
