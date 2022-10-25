0701ad342e32bc9fac358518b566c99f7d4f62a4
Eugene Kofman <eugenekofman@gmail.com>
2022-10-25 14:36:45 -0400

    add usestate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getCurrentUser } from "./store/session";
 
 function App() {
