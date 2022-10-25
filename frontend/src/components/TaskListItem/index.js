6ee51352074470b002aca1db0a1ffd4d04ba4ca0
Wilson Chen <wilsonchen901@gmail.com>
2022-10-24 21:27:31 -0400

    style checkbox

--- frontend/src/components/TaskListItem/index.js
+++ frontend/src/components/TaskListItem/index.js
@@ -4,6 +4,7 @@ import './TaskListItem.css';
 const TaskListItem = ({task}) => {
 
     const [showOptions, setShowOptions] = useState(false);
+    const [checked, setChecked] = useState(false);
 
     const handleOptions = (e) => {
         e.preventDefault();
@@ -16,7 +17,26 @@ const TaskListItem = ({task}) => {
         >
             <div className='task-item-body'>
                 <div className='task-item-body-start'>
-                    <div className='task-item-check'><input type="checkbox"/></div>
+                    <div className='task-item-check'>
+                        <label>
+                            <input type="checkbox"
+                            className='task-checkbox'
+                            onChange={() => setChecked(!checked)}
+                            />
+                            <svg
+                                className={`checkbox ${checked ? "checkbox--active" : ""}`}
+                                aria-hidden="true"
+                                viewBox="0 0 15 11"
+                                fill="none"
+                            > 
+                                <path 
+                                    d="M1 4.5L5 9L14 1"
+                                    strokeWidth="2"
+                                    stroke={checked ? "#fff" : "none"}
+                                />
+                            </svg>
+                        </label>
+                    </div>
                     <div className='task-item-title'>{task.title}</div>
                 </div>
                 {showOptions && <div className='task-item-options'
