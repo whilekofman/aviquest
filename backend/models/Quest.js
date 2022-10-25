91edeb5baedfe50fe0285e04e5b4d4e37b76e228
Eugene Kofman <eugenekofman@gmail.com>
2022-10-25 17:42:19 -0400

    backend work

--- backend/models/Quest.js
+++ backend/models/Quest.js
@@ -1,28 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const taskSchema = Schema({
+const questSchema = Schema({
     title: {
         type: String,
         required: true
     },
-    body: {
-        type: String
-    },
-    isComplete: {
-        type: Boolean,
-        default: false,
-        required: true
-    },
-    difficulty: {
-        type: Number,
-        default: 1,
+    description: {
+        type: String,
         required: true
 
+    },
+    // rewards: {
+
+    // }
+
+    difficulty: {
+        type: Number,
+        required: true
     },
 
 }, {
     timestamps: true
 });
 
-module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model('Quest', questSchema);
