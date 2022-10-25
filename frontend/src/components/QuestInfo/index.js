53b483b9ce7126c7b9691fbfa17f223beef248d2
Wilson Chen <wilsonchen901@gmail.com>
2022-10-25 18:50:34 -0400

    style quest info

--- frontend/src/components/QuestInfo/index.js
+++ frontend/src/components/QuestInfo/index.js
@@ -4,17 +4,24 @@ const QuestInfo = ({currentQuest}) => {
 
     console.table(currentQuest)
 
+
+
     return ( 
         <div className='quest-info-container'>
-            <div className='quest-info-window'>
-                <img src={currentQuest.monster.imageUrl} 
-                className="quest-window-image"
-                />
+            <div className='quest-info-top'>
+                <div className='quest-info-window'>
+                    <img src={currentQuest.monster.imageUrl} 
+                    className="quest-window-image"
+                    />
+                </div>
+                <div className='quest-info-details-container'>
+                    <div className='quest-info-title'>{currentQuest.monster.name}</div>
+                    <div className='quest-info-monster-health'>HP: {currentQuest.monster.health}</div>
+                    <div className='quest-info-monster-attack'>ATK: {currentQuest.monster.attack}</div>
+                    <div className='quest-info-description'>{currentQuest.description}</div>
+                </div>
             </div>
-            <div className='quest-info-details-container'>
-                <div className='quest-info-title'>{currentQuest.title}</div>
-                <div className='quest-info-description'>{currentQuest.description}</div>
-            </div>
+            <div className='quest-info-details'>{currentQuest.text}</div>
         </div>
      );
 }
