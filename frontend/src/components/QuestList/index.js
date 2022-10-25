6233f546991f022422c4dc02a7d740706109f54a
Wilson Chen <wilsonchen901@gmail.com>
2022-10-25 09:59:16 -0400

    start quest list item

--- frontend/src/components/QuestList/index.js
+++ frontend/src/components/QuestList/index.js
@@ -1,26 +1,10 @@
-import QuestInfo from '../QuestInfo';
 import QuestListItem from '../QuestListItem';
 import './QuestList.css';
 
-const QuestList = ({quests}) => {
-
-    const questItem = quests.map(quest => <QuestListItem key={quest.id} quest={quest}/>)
-
+const QuestList = () => {
     return ( 
-        <div className='quest-panel'>
-            <div className='quest-list-container'>
-                    <div className='quest-block'></div>
-                <div className='quest-body'>
-                    <ul className='quest-ul'>
-                        {questItem}
-                    </ul>
-                </div>
-            </div>
-
-            <div className='quest-panel-info'>
-                <QuestInfo quest={questItem} />
-
-            </div>
+        <div className='questlist-container'>
+            <div className='quest-body'>Quest Body</div>
         </div>
      );
 }
