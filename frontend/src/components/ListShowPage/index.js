9be9efbe0b2ab7d7184e1a8c86b7ab75f2009547
Wilson Chen <wilsonchen901@gmail.com>
2022-10-25 13:55:02 -0400

    build basic quest store and reducer

--- frontend/src/components/ListShowPage/index.js
+++ frontend/src/components/ListShowPage/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import QuestInfo from '../QuestInfo';
 import QuestList from '../QuestList';
 import TaskList from '../TaskList';
@@ -40,6 +41,12 @@ const ListShowPage = () => {
     quest: null
     }
 
+    const quests = [
+        {title: "Slime Hunter", description: "Kill 5 Slimes", reward: {coin: 5, item: null}, timeFrame: 3, id: 1},
+        {title: "Goblin Hunter", description: "Kill 3 Goblins", reward: {coin: 10, item: "Scuffed Dagger"}, timeFrame: 2, id: 2},
+        {title: "Orc Disaster", description: "Kill 1 Orc General", reward: {coin: 5, item: "Orc General's Chipped Warblade"}, timeFrame: 4, id: 3}
+    ]
+
     return ( 
         <div className='list-show-container'>
             <div className='todo-section'>
@@ -52,12 +59,12 @@ const ListShowPage = () => {
 
             <div className='quest-section'>
                 <div className='quest-header'>Quest</div>
-                <QuestList />
+                <QuestList quests={quests}/>
             </div>
 
             <div className='quest-info-section'>
                 <div className='quest-info-header'>Quest Info</div>
-                <QuestInfo />
+                <QuestInfo quests={quests} />
             </div>
         </div>
      );
---
+++ frontend/src/store/quest.js
@@ -0,0 +1,39 @@
+const RECEIVE_QUEST = 'quest/RECEIVE_QUEST';
+const REMOVE_QUEST = 'quest/REMOVE_QUEST';
+
+const selectQuest = quest => ({
+    type: RECEIVE_QUEST,
+    quest
+})
+
+const deselectQuest = questId => ({
+    type: REMOVE_QUEST,
+    questId
+})
+
+export const getQuest = questId => ({quests}) => quests ? quests[questId] : null;
+
+export const setQuest = (quest) => dispatch => {
+    dispatch(selectQuest(quest));
+}
+
+export const removeQuest = (questId) => dispatch => {
+    dispatch(deselectQuest(questId));
+}
+
+const questReducer = (state = {}, action) => {
+    Object.freeze(state);
+    const nextState = {...state};
+
+    switch (action.type) {
+        case RECEIVE_QUEST:
+            nextState[action.quest.id] = action.quest;
+        case REMOVE_QUEST:
+            delete nextState[action.questId];
+            return nextState;
+        default:
+            return state;
+    }
+}
+
+export default questReducer;
