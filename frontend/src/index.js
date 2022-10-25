f38311586db561afbc81481b9450e00b6789fd43
Han Chen <hanmikechen@gmail.com>
2022-10-25 11:13:45 -0400

    fix index.js provider and store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalProvider } from './context/Modal';
-// import configureStore from "./store";
+import configureStore from './store/store'
 
 function Root() {
   const store = configureStore();
 
   return (
     <ModalProvider>
-        {/* <Provider store={store}> */}
+        <Provider store={store}>
           <BrowserRouter>
             <App />
           </BrowserRouter>
-        {/* </Provider> */}
+        </Provider>
     </ModalProvider>
-  );
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-
-let store = configureStore({})
-
-function Root(){
-  const store = configureStore();
-
-  return (
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
   );
 }
 
