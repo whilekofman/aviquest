d4133417008d4c61038deb2d021415c3c8b171a9
Wilson Chen <wilsonchen901@gmail.com>
2022-10-25 09:59:16 -0400

    start quest list item

--- frontend/src/components/QuestList/index.js
+++ frontend/src/components/QuestList/index.js
@@ -1,9 +1,23 @@
+import QuestListItem from '../QuestListItem';
 import './QuestList.css';
 
 const QuestList = () => {
+
+    const quests = [
+        {title: "Slime Hunter", description: "Kill 5 Slimes", reward: {coin: 5, item: null}, timeFrame: 3},
+        {title: "Goblin Hunter", description: "Kill 3 Goblins", reward: {coin: 10, item: "Scuffed Dagger"}, timeFrame: 2},
+        {title: "Orc Disaster", description: "Kill 1 Orc General", reward: {coin: 5, item: "Orc General's Chipped Warblade"}, timeFrame: 4}
+    ]
+
+    const questItem = quests.map(quest => <QuestListItem quest={quest}/>)
+
     return ( 
         <div className='questlist-container'>
-            <div className='quest-body'>Quest Body</div>
+            <div className='quest-body'>
+                <ul>
+                    {questItem}
+                </ul>
+            </div>
         </div>
      );
 }
