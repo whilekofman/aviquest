a377d119dbbba225dffd295b39bfc14c10d3b071
Eugene Kofman <eugenekofman@gmail.com>
2022-10-25 14:28:25 -0400

    add current user check to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,18 @@ import StatusBar from "./components/StatusBar";
 import { Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getCurrentUser } from "./store/session";
 
 function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(()=> setLoaded(true));
+  }, [dispatch])
+
+  return loaded && (
     <>
       <Navigation />
     {/* <SplashPage /> */}
