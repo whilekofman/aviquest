788fde3a8a0d470bcd223e4a8bd3d6918aecb94d
Han Chen <hanmikechen@gmail.com>
2022-10-26 10:39:45 -0400

    add equipment bar and inventory modal

---
+++ frontend/src/components/EquipmentBar/index.js
@@ -0,0 +1,9 @@
+import './EquipmentBar.css'
+
+const EquipmentBar = () => {
+    return (
+        <h1>Equipment Bar</h1>
+    );
+}
+ 
+export default EquipmentBar;
---
+++ frontend/src/components/Inventory/index.js
@@ -0,0 +1,20 @@
+import './Inventory.css'
+
+const Inventory = () => {
+    return (
+        <div className='inventory-modal'>
+            <div className='inventory-equipment'>
+                
+            </div>
+            <div className='inventory-index'>
+                <ul className='inventory-list'>
+                    <li className='inventory-item'>
+                        <h1>iventory item</h1>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+ 
+export default Inventory;
