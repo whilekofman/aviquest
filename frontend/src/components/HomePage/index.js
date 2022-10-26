7f6111bc8c840c9948126e008c06bbe220d3eb9d
DLi53 <106249295+DLi53@users.noreply.github.com>
2022-10-26 19:53:13 -0400

    change routes

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,27 @@
+import './HomePage.css'
+import { useSelector } from "react-redux";
+import ListShowPage from "../ListShowPage";
+import SplashPage from "../SplashPage";
+import StatusBar from "../StatusBar";
+
+const HomePage = () => {
+    const currentUser = useSelector(state => state.session.user)
+
+    const notLoggedIn = (
+        <div className="HomePage">
+            <SplashPage/>
+        </div>
+    )
+
+    const yesLoggedIn = (
+        <div className="HomePage">
+            <StatusBar/>
+            <ListShowPage />
+        </div>
+    )
+
+    return currentUser ? yesLoggedIn : notLoggedIn
+    
+}
+ 
+export default HomePage;
