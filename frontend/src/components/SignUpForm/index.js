d8152909c63b61dcd360c3d01a791e9798b968cd
Han Chen <hanmikechen@gmail.com>
2022-10-26 14:19:07 -0400

    add inventory seed

--- frontend/src/components/SignUpForm/index.js
+++ frontend/src/components/SignUpForm/index.js
@@ -81,7 +81,7 @@ function SignUpForm() {
 
           <br />
           
-          <label htmlFor="signup-password-input">Password:</label>
+          <label htmlFor="signup-password">Password:</label>
           <input
             className="signup-password-input"
             id='signup-password'
