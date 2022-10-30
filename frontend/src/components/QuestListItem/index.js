cf7ace944ae7c6085547b9f767e5fb5ed6ca58b0
Wilson Chen <wilsonchen901@gmail.com>
2022-10-30 17:10:20 -0400

    clean up questlistitem code

--- frontend/src/components/QuestListItem/index.js
+++ frontend/src/components/QuestListItem/index.js
@@ -7,18 +7,9 @@ import './QuestListItem.css';
 
 const QuestListItem = ({quest, setCurrentQuest}) => {
 
-    // const [loaded, setLoaded] = useState(false);
     const dispatch = useDispatch();
     const {_id, username, email, items, equipment, attack, coins, maxHealth, currentHealth } = useSelector(state => state.session.user);
 
-    // useEffect(() => {
-    //     // dispatch(sessionActions.getCurrentUser());
-    //     // const updateUser = () => {
-            
-    //     // }
-
-    // }, [loaded])
-    
     const handleClick = (e) => {
         e.preventDefault();
         setCurrentQuest(quest);
@@ -34,11 +25,7 @@ const QuestListItem = ({quest, setCurrentQuest}) => {
             currentHealth,
             quest: [quest]
         }
-        
         dispatch(userActions.updateUser(userData));
-        
-        // dispatch(userActions.updateUser(userData));
-        // setLoaded(!loaded);
     }
 
     return ( 
