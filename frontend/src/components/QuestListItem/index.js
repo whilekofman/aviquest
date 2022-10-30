11d8978051a84a1c2eb8e3a2d97e7ed3cd0dfb1f
Wilson Chen <wilsonchen901@gmail.com>
2022-10-30 17:09:29 -0400

    fix handleClick(e)

--- frontend/src/components/QuestListItem/index.js
+++ frontend/src/components/QuestListItem/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as userActions from '../../store/user';
 import * as sessionActions from '../../store/session';
 import * as questActions from '../../store/quest';
@@ -7,9 +7,17 @@ import './QuestListItem.css';
 
 const QuestListItem = ({quest, setCurrentQuest}) => {
 
+    // const [loaded, setLoaded] = useState(false);
     const dispatch = useDispatch();
     const {_id, username, email, items, equipment, attack, coins, maxHealth, currentHealth } = useSelector(state => state.session.user);
 
+    // useEffect(() => {
+    //     // dispatch(sessionActions.getCurrentUser());
+    //     // const updateUser = () => {
+            
+    //     // }
+
+    // }, [loaded])
     
     const handleClick = (e) => {
         e.preventDefault();
@@ -27,10 +35,10 @@ const QuestListItem = ({quest, setCurrentQuest}) => {
             quest: [quest]
         }
         
+        dispatch(userActions.updateUser(userData));
+        
         // dispatch(userActions.updateUser(userData));
-        dispatch(questActions.setQuest(quest, userData));
-        dispatch(sessionActions.getCurrentUser());
-
+        // setLoaded(!loaded);
     }
 
     return ( 
