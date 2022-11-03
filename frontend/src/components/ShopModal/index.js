908f6bd08e99002b9c43b8eb3ad8bfc7ade64b32
Han Chen <hanmikechen@gmail.com>
2022-11-03 17:45:17 -0400

    implement gacha with random timing and timeout

--- frontend/src/components/ShopModal/index.js
+++ frontend/src/components/ShopModal/index.js
@@ -1,37 +1,50 @@
-import { Modal } from '../../context/Modal';
 import './ShopModal.css'
-import ShopItem from './ShopItem'
 import itemList from '../Util/item_data';
-import gachapon from '../../assets/images/gachapon.gif'
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import gachapon from '../../assets/images/gachapon.gif';
+import ShopItem from './ShopItem';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { updateUser } from '../../store/user';
-import { getQuests } from '../../store/quest';
+
 
 const ShopModal = () => {
-
-    const dispatch = useDispatch();
-    // const allItemsMapped = itemData.map(item => <ShopItem item={item} key={item.id}/> )
-    let i = 0;
-    // const itemData = itemList[setInterval((i) => {
-    //     if (i > itemList.length - 2) {
-    //         return 0;
-    //     } else {
-    //         return i + 1;
-    //     }
-    // },1)];
-
-
+    
     const [itemData, setItemData] = useState(null);
+    let [index, setIndex] = useState(0);
+    let [moving, setMoving] = useState(false);
     const user = useSelector(state => state.session.user);
+    const {_id, coins} = user;
+    // const [coins, setCoins] = useState(user.coins)
+    const dispatch = useDispatch();
+    
+    let timeout;    
 
+    useEffect(() => {
+        if (moving) {
+            setItemData(itemList[index]);
+            const count = setInterval(() => {
+                if (index > itemList.length - 2) {
+                    setIndex(0);
+                } else {
+                    setIndex(index + 1)
+                }
+            },70);
+            return () => {
+                clearInterval(count);
+            };
+        }
+    },[index,moving]);
+    clearTimeout(timeout);
     const handleClick = (e) => {
         e.preventDefault();
-        let randomNumber = Math.floor(Math.random() * itemList.length);
-        setItemData(itemList[randomNumber]);
+        setMoving(true);
+        timeout = setTimeout(() => {
+            setMoving(false);
+        },Math.random() * (300) + 1700);
+        dispatch(updateUser({coins: coins -50, _id}));
     };
-
-    console.log(itemData)
 
     return ( 
         <div className="ShopModal">
@@ -39,15 +52,26 @@ const ShopModal = () => {
             <div className='gacha-cost'>
                 50 coins / crank
             </div>
-            <div className='gacha-item-box'>
+
+            <div id='gacha-item-box' className='gacha-item-box'>
+                {/* <div>
+                    aaa
+                </div>
+                <div>
+                    bbb
+                </div>
+                <div>
+                    abc
+                </div> */}
+
                 {itemData && (
                     <img className='gacha-item-image' src={itemData.imageUrl} alt="" />
                 )}
-                
             </div>
+
             <div className='gacha-item-name'>
                 {itemData && (
-                        itemData.name
+                    itemData.name
                 )}
             </div>
 
@@ -55,10 +79,6 @@ const ShopModal = () => {
                 <img className='big-gacha-button'
                 onClick={handleClick} src={gachapon} alt="" />
             </div>
-
-            {/* <div className="ShowModal-ShopItems">
-                {allItemsMapped}
-            </div> */}
 
             
         </div>
