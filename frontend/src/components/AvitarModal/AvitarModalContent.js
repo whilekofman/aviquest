1d6cdb5a9f55222e500d8cd881eb4a81a3c1715c
DLi53 <106249295+DLi53@users.noreply.github.com>
2022-11-08 18:58:30 -0500

    avitar edit

--- frontend/src/components/AvitarModal/AvitarModalContent.js
+++ frontend/src/components/AvitarModal/AvitarModalContent.js
@@ -4,7 +4,7 @@ import AvitarStats from "../StatusBar/AvitarStats";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import EquipmentBar from "../EquipmentBar";
-
+import avitars from '../Util/avitar_data.js'
 
 
 const AvitarModalContent = () => {
@@ -13,11 +13,18 @@ const AvitarModalContent = () => {
     const currentUser = useSelector(state => state.session.user)
     const [avitarPicURL, setavitarPicURL] = useState('currentUserURl')
 
-    const avitar1 = 'https://www.pngfind.com/pngs/m/55-557958_iron-man-final-fantasy-pixel-art-hd-png.png'
+    console.log(avitars);
+
+    const avitar1 = {
+        name:'avitar1',
+        avitar:'https://www.pngfind.com/pngs/m/55-557958_iron-man-final-fantasy-pixel-art-hd-png.png',
+        movingImageUrl: ''
+    }
     const avitar2 = 'https://img.favpng.com/24/10/8/thor-pixel-art-marvel-cinematic-universe-loki-png-favpng-L9NYWsCxgygXbYPNgXF6aky4a.jpg'
     const avitar3 = 'https://www.pngkey.com/png/detail/164-1648335_hulk-smash-megaman-8-bit.png'
     const avitar4 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg12pnpP8jz2a5PvsmWm3pU8xsnzPD9xkyF2liPAHMuZedYeIJN9aKD1zY3tuwPDDJqes&usqp=CAU'
 
+    const avitars_mapped = avitars.map(avitar => <img className='AvitarModalPicsEditPic' src={avitar.avitar} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar.avitar)}/>)
 
     const show = ( 
         <div  onClick={(e)=> e.stopPropagation()} className="AvitarModalContent" >
@@ -43,39 +50,44 @@ const AvitarModalContent = () => {
             </div>
             <br />
 
-            {/* <button className="AvitarModalEditButton" onClick={(e) => setEditing(true)}>Edit</button> */}
+            <button className="AvitarModalEditButton" onClick={(e) => setEditing(true)}>Edit</button>
         </div>
      );
 
      const edit = (
         <form onClick={(e)=> e.stopPropagation()} className="AvitarModalContent" >
             <div className="AvitarEditing"> Editing: </div>
+
                 <div className="AvitarModalPicsEdit">
                     <label htmlFor=""></label>
-                    <img className='AvitarModalPicsEditPic' src={avitar1} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar1)}/>
+                    {/* <img className='AvitarModalPicsEditPic' src={avitar1} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar1)}/>
                     <img className='AvitarModalPicsEditPic'src={avitar2} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar2)}/>
                     <img className='AvitarModalPicsEditPic'src={avitar3} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar3)}/>
+                    <img className='AvitarModalPicsEditPic'src={avitar3} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar3)}/>
+                    <img className='AvitarModalPicsEditPic'src={avitar3} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar3)}/> */}
+
                     {/* <img src={avitar4} alt="" value='google.com' onClick={(e) => setavitarPicURL(avitar4)}/> */}
 
+                    {avitars_mapped}
                 </div>
+
+                <select>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+</select>
 
                 <br />
                 <AvitarStats user={currentUser} id="AvitarModalStats"/>
-                {currentUser.email}
-                <input type="email" placeholder="Enter New Email"/> 
-                {currentUser.atk}
-                {currentUser.coins}
+ 
                 <br />
-                Aviquips:
-                <EquipmentBar/>
-                <br />
-
+                <button className="AvitarModalEditButton" onClick={(e) => setEditing(false)}>Save Changes</button>
                 <button className="AvitarModalEditButton" onClick={(e) => setEditing(false)}>Save Changes</button>
         </form>
 
      )
 
-     return editing ? show : show
+     return editing ? edit : show
 }
  
 export default AvitarModalContent;
---
+++ frontend/src/components/Util/avitar_data.js
@@ -0,0 +1,29 @@
+const avitar1 = {
+    name:'avitar1',
+    avitar:'https://www.pngfind.com/pngs/m/55-557958_iron-man-final-fantasy-pixel-art-hd-png.png',
+    movingImageUrl: '',
+    cost: 50
+}
+const avitar2 = {
+    name:'avitar2',
+    avitar:'https://img.favpng.com/24/10/8/thor-pixel-art-marvel-cinematic-universe-loki-png-favpng-L9NYWsCxgygXbYPNgXF6aky4a.jpg',
+    movingImageUrl: '',
+    cost: 50
+}
+
+const avitar3 = {
+    name:'avitar1',
+    avitar:'https://www.pngkey.com/png/detail/164-1648335_hulk-smash-megaman-8-bit.png',
+    movingImageUrl: '',
+    cost: 50
+}
+
+
+// const avitar2 = 'https://img.favpng.com/24/10/8/thor-pixel-art-marvel-cinematic-universe-loki-png-favpng-L9NYWsCxgygXbYPNgXF6aky4a.jpg'
+// const avitar3 = 'https://www.pngkey.com/png/detail/164-1648335_hulk-smash-megaman-8-bit.png'
+const avitar4 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg12pnpP8jz2a5PvsmWm3pU8xsnzPD9xkyF2liPAHMuZedYeIJN9aKD1zY3tuwPDDJqes&usqp=CAU'
+
+const avitars = [avitar1, avitar2, avitar3]
+
+export default avitars
+
