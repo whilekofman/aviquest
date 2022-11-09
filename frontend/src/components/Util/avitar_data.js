07d7d82fb348134226d513af6fda602672995bfd
DLi53 <106249295+DLi53@users.noreply.github.com>
2022-11-09 12:14:38 -0500

    edit user avitar

--- frontend/src/components/Util/avitar_data.js
+++ frontend/src/components/Util/avitar_data.js
@@ -1,18 +1,18 @@
 const avitar1 = {
     name:'avitar1',
-    avitar:'https://www.pngfind.com/pngs/m/55-557958_iron-man-final-fantasy-pixel-art-hd-png.png',
-    movingImageUrl: '',
+    avitar:'https://aviquest-dev.s3.amazonaws.com/Avitars/char1prof.gif',
+    movingImageUrl: 'https://aviquest-dev.s3.amazonaws.com/Avitars/char1move.gif',
     cost: 50
 }
 const avitar2 = {
     name:'avitar2',
     avitar:'https://img.favpng.com/24/10/8/thor-pixel-art-marvel-cinematic-universe-loki-png-favpng-L9NYWsCxgygXbYPNgXF6aky4a.jpg',
-    movingImageUrl: '',
+    movingImageUrl: 'https://img.favpng.com/24/10/8/thor-pixel-art-marvel-cinematic-universe-loki-png-favpng-L9NYWsCxgygXbYPNgXF6aky4a.jpg',
     cost: 50
 }
 
 const avitar3 = {
-    name:'avitar1',
+    name:'avitar3',
     avitar:'https://www.pngkey.com/png/detail/164-1648335_hulk-smash-megaman-8-bit.png',
     movingImageUrl: '',
     cost: 50
