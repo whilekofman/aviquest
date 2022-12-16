2ef9bd78db36c7c29889609bb8288210df7d7497
Han Chen <hanmikechen@gmail.com>
2022-12-16 16:14:49 -0500

    fix devpage styling

--- frontend/src/components/DevTeam/Index.js
+++ frontend/src/components/DevTeam/Index.js
@@ -8,18 +8,19 @@ const DevTeam = () => {
     return (  
     
     <div className="team-container-inner">
-
-        <div className="engineer-container">
-            <Eugene />
-        </div>
-        <div className="engineer-container">
-            <Wilson />
-        </div>
-        <div className="engineer-container">
-            <Daniel />
-        </div>
-        <div className="engineer-container">
-            <Han />
+        <div className="team-container-wrapper">
+            <div className="engineer-container">
+                <Eugene />
+            </div>
+            <div className="engineer-container">
+                <Wilson />
+            </div>
+            <div className="engineer-container">
+                <Daniel />
+            </div>
+            <div className="engineer-container">
+                <Han />
+            </div>
         </div>
     </div>
     );
